1a0e4d812c58042e9f758a8d9a4a6591237e04f8
Angel Gallego <angelgb1990@gmail.com>
2024-10-18 19:21:55 +0000

    creadas ramas

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div>
+            <h1>Titulo</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <button type="button" class="btn btn-primary">Primary</button>
+        </div>
+    )
+}
+export default Jumbotron
